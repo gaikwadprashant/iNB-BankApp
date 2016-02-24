95beec9f64fa0b196a7e038e12a6c5bb3be51a3a
gaikwad_p <mailprashantgaikwad@gmail.com>
2016-02-24 15:33:49 +0530

    Required dependendcy added

--- src/main/java/com/inbbank/App.java
+++ src/main/java/com/inbbank/App.java
@@ -2,12 +2,16 @@ package com.inbbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
+@SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
-@ComponentScan("beans.xml")
+@ComponentScan
+@ImportResource(value="beans.xml")
 public class App 
 {
     public static void main( String[] args )
