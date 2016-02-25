ff93c82bbbc0e69907d9c45fb288ae9e0389e28f
Arun <findarun2002@gmail.com>
2016-02-25 14:31:16 +0530

    updated

---
+++ src/main/java/com/inbbank/wsentity/WSAccount.java
@@ -0,0 +1,80 @@
+package com.inbbank.wsentity;
+
+import java.math.BigDecimal;
+
+public class WSAccount {
+
+	private String id;
+
+	private BigDecimal accountNumber;
+
+	private String accountType;
+
+	private BigDecimal balance;
+
+	private BigDecimal interestRate;
+
+
+
+
+	public String getId() {
+		return id;
+	}
+
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+
+	public BigDecimal getAccountNumber() {
+		return accountNumber;
+	}
+
+
+
+	public void setAccountNumber(BigDecimal accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+
+
+	public String getAccountType() {
+		return accountType;
+	}
+
+
+
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+
+
+
+	public BigDecimal getBalance() {
+		return balance;
+	}
+
+
+
+	public void setBalance(BigDecimal balance) {
+		this.balance = balance;
+	}
+
+
+
+	public BigDecimal getInterestRate() {
+		return interestRate;
+	}
+
+
+
+	public void setInterestRate(BigDecimal interestRate) {
+		this.interestRate = interestRate;
+	}
+
+
+
+}
