1f4c03d3429dbf7c9d43d6ea0ca7d6f8bdb000b3
gaikwad_p <mailprashantgaikwad@gmail.com>
2016-02-29 15:50:53 +0530

    upload document

---
+++ src/main/java/com/inbbank/BankingAppInitializer.java
@@ -0,0 +1,36 @@
+package com.inbbank;
+
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class BankingAppInitializer extends
+		AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] { BankingConfiguration.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+	@Override
+	protected void customizeRegistration(
+			ServletRegistration.Dynamic registration) {
+
+		MultipartConfigElement multipartConfigElement = new MultipartConfigElement(
+				"/", 100000, 200000, 50000);
+
+		registration.setMultipartConfig(multipartConfigElement);
+
+	}
+}
